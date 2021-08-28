85451710d1bc65fd61aa985ba0ec795993ce0fa2
Masa <katsukatsu.masa.0522@gmail.com>
2021-08-29 01:52:33 +0900

    data add

--- assets/data/users.js
+++ assets/data/users.js
@@ -11,4 +11,34 @@ export default [
     userIntroduction: 'IT勤務',
     image: 'https://girlydrop.com/wp-content/uploads/post/p82.jpg',
   },
+  {
+    id: '2',
+    userName: 'Nene',
+    userIntroduction: '大学生',
+    image: 'https://girlydrop.com/wp-content/uploads/post/p8131.jpg',
+  },
+  {
+    id: '3',
+    userName: 'Anna',
+    userIntroduction: 'IT勤務',
+    image: 'https://girlydrop.com/wp-content/uploads/post/p82.jpg',
+  },
+  {
+    id: '4',
+    userName: 'Nene',
+    userIntroduction: '大学生',
+    image: 'https://girlydrop.com/wp-content/uploads/post/p8131.jpg',
+  },
+  {
+    id: '5',
+    userName: 'Anna',
+    userIntroduction: 'IT勤務',
+    image: 'https://girlydrop.com/wp-content/uploads/post/p82.jpg',
+  },
+  {
+    id: '6',
+    userName: 'Nene',
+    userIntroduction: '大学生',
+    image: 'https://girlydrop.com/wp-content/uploads/post/p8131.jpg',
+  },
 ];

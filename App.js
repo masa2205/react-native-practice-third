c3242b33d468094088523a4c9fe7689f0461a6e5
Masa <katsukatsu.masa.0522@gmail.com>
2021-08-29 01:52:26 +0900

    stack of card edit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -15,12 +15,19 @@ import Animated, {
   useDerivedValue,
   useAnimatedGestureHandler,
   interpolate,
+  withSpring,
+  runOnJS,
 } from 'react-native-reanimated';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 
 const ROTATION = 60;
+const SWIPE_VELOCITY = 800;
 
 const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
   const {width: screenWidth} = useWindowDimensions();
   const hiddenTranslateX = 2 * screenWidth;
   const translateX = useSharedValue(0);
@@ -39,6 +46,22 @@ const App = () => {
       },
     ],
   }));
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.7, 1],
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.6, 1],
+    ),
+  }));
   const gesturehandler = useAnimatedGestureHandler({
     onStart: (_, context) => {
       context.startX = translateX.value;
@@ -46,17 +69,42 @@ const App = () => {
     onActive: (event, context) => {
       translateX.value = context.startX + event.translationX;
     },
-    onEnd: () => {
-      console.warn('Touch ended');
+    onEnd: event => {
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        translateX.value = withSpring(0);
+        return;
+      }
+
+      translateX.value = withSpring(
+        hiddenTranslateX * Math.sign(event.velocityX),
+        {},
+        () => runOnJS(setCurrentIndex)(currentIndex + 1),
+      );
     },
   });
+
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
+
   return (
     <View style={styles.container}>
-      <PanGestureHandler onGestureEvent={gesturehandler}>
-        <Animated.View style={[styles.animatedCard, cardStyle]}>
-          <Card user={users[0]} />
-        </Animated.View>
-      </PanGestureHandler>
+      {nextProfile && (
+        <View style={styles.nextCardContainer}>
+          <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+            <Card user={nextProfile} />
+          </Animated.View>
+        </View>
+      )}
+
+      {currentProfile && (
+        <PanGestureHandler onGestureEvent={gesturehandler}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <Card user={currentProfile} />
+          </Animated.View>
+        </PanGestureHandler>
+      )}
     </View>
   );
 };
@@ -68,6 +116,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   animatedCard: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',

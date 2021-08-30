cfff70fabcf568690e4af7e23b73a1ba3487bfe7
Masa <katsukatsu.masa.0522@gmail.com>
2021-08-30 20:43:37 +0900

    button

---
+++ src/components/AnimatedStack/index.js
@@ -0,0 +1,167 @@
+import 'react-native-gesture-handler';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, useWindowDimensions, Text} from 'react-native';
+import Card from './src/components/';
+import users from './assets/data/users';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+  useAnimatedGestureHandler,
+  interpolate,
+  withSpring,
+  runOnJS,
+} from 'react-native-reanimated';
+import {PanGestureHandler} from 'react-native-gesture-handler';
+import Like from './assets/images/LIKE.png';
+import Nope from './assets/images/nope.png';
+import TopBar from './src/components/TopBar';
+import UnderBar from './src/components/UnderBar';
+
+const ROTATION = 60;
+const SWIPE_VELOCITY = 800;
+
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
+  const {width: screenWidth} = useWindowDimensions();
+  const hiddenTranslateX = 2 * screenWidth;
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () =>
+      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
+      'deg',
+  );
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      },
+    ],
+  }));
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.7, 1],
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.6, 1],
+    ),
+  }));
+
+  const likeStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(translateX.value, [0, hiddenTranslateX / 5], [0, 1]),
+  }));
+  const nopeStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(translateX.value, [0, -hiddenTranslateX / 5], [0, 1]),
+  }));
+
+  const gesturehandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
+    },
+    onEnd: event => {
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        translateX.value = withSpring(0);
+        return;
+      }
+
+      translateX.value = withSpring(
+        hiddenTranslateX * Math.sign(event.velocityX),
+        {},
+        () => runOnJS(setCurrentIndex)(currentIndex + 1),
+      );
+    },
+  });
+
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
+
+  function handleLikePress() {}
+  function handlePassPress() {}
+
+  return (
+    <View style={styles.container}>
+      <TopBar />
+      {nextProfile && (
+        <View style={styles.nextCardContainer}>
+          <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+            <Card user={nextProfile} />
+          </Animated.View>
+        </View>
+      )}
+
+      {currentProfile && (
+        <PanGestureHandler onGestureEvent={gesturehandler}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <Animated.Image
+              source={Like}
+              style={[styles.like, {left: 10}, likeStyle]}
+              resizeMode="contain"
+            />
+            <Animated.Image
+              source={Nope}
+              style={[styles.like, {right: 10}, nopeStyle]}
+              resizeMode="contain"
+            />
+            <Card user={currentProfile} />
+          </Animated.View>
+        </PanGestureHandler>
+      )}
+      <UnderBar
+        handleLikePress={handleLikePress}
+        handlePassPress={handlePassPress}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  topContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  animatedCard: {
+    width: '90%',
+    height: '70%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  like: {
+    width: 150,
+    height: 150,
+    position: 'absolute',
+    top: 10,
+    zIndex: 1,
+    elevation: 1,
+  },
+});
+
+export default App;
